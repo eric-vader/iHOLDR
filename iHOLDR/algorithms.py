7c05bebefa4503c19150fa7eda57a33a04b8f279
Eric Han <Eric.aka.darth_vader@hotmail.com>
2022-03-15 15:47:50 +0800

    inital implementation

--- iHOLDR/algorithms.py
+++ iHOLDR/algorithms.py
@@ -1,31 +1,118 @@
 #!/usr/bin/env python3
 import common
 import logging
-import george
 import numpy as np
+
+import george
+import sklearn
+from sklearn.gaussian_process import GaussianProcessRegressor
+import GPy
+import torch
+import gpytorch
+
 
 class ExampleAlgorithm(common.Component):
     def __init__(self, datasets, **kwargs):
         super().__init__(**kwargs)
         logging.info(kwargs)
-        print(datasets)
+        logging.info(datasets)
 
-class GeorgeGP(common.Component):
-    def __init__(self, datasets, solver, kernel, kernel_kwargs, **kwargs):
+class CommonGP(common.Component):
+    kernel_kwargs_mapper = {}
+    def kernel_kwargs_adaptor(self, kernel_kwargs):
+        return { (self.kernel_kwargs_mapper[k] if k in self.kernel_kwargs_mapper else k) : v for k,v in kernel_kwargs.items()  }
+    def __init__(self, datasets, kernel_kwargs, **kwargs):
+        super().__init__(**kwargs)
+        self.rng = np.random.default_rng(self.random_seed)
+        
+        self.datasets = datasets
+        self.data = datasets.generate_data()
 
-        N = datasets.N
-        X = datasets.X
-        y = datasets.y
-        var_y = datasets.var_y
-        yerr = datasets.yerr
+        if kernel_kwargs['variance'] == 'population':
+            kernel_kwargs['variance'] = np.var(self.data.y)
+        self.kernel_kwargs = self.kernel_kwargs_adaptor(kernel_kwargs)
 
-        kernel = var_y * getattr(george.kernels, kernel)(**kernel_kwargs)
+class GeorgeGP(CommonGP):
+    kernel_kwargs_mapper = {
+        'lengthscale':'metric'
+    }
+    def __init__(self, solver, kernel, noise_variance, **kwargs):
+        super().__init__(**kwargs)
+
+        noise = np.sqrt(noise_variance)
+
+        N = self.data.N
+        X = self.data.X
+        y = self.data.y
+
+        yerr = noise * np.ones_like(X)
+
+        var_y = self.kernel_kwargs.pop('variance')
+
+        kernel = var_y * getattr(george.kernels, kernel)(**self.kernel_kwargs)
 
         idx = np.array(range(N))
         _gp_hodlr = george.GP(kernel, solver=getattr(george, solver))
         _gp_hodlr.compute(X[idx], yerr[idx])
-        print(_gp_hodlr.log_likelihood(y[idx]))
+        logging.info(_gp_hodlr.log_likelihood(y[idx]))
 
         gp_basic = george.GP(kernel)
-        gp_basic.compute(X[:N], yerr[:N])
-        print(gp_basic.log_likelihood(y[:N]))
+        gp_basic.compute(X, yerr)
+        logging.info(gp_basic.log_likelihood(y))
+
+        self.data.reshape()
+
+        train_x = torch.from_numpy(X)
+        train_y = torch.from_numpy(y)
+        class ExactGPModel(gpytorch.models.ExactGP):
+            def __init__(self, train_x, train_y, likelihood):
+                super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+                self.mean_module = gpytorch.means.ConstantMean()
+                self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+            def forward(self, x):
+                mean_x = self.mean_module(x)
+                covar_x = self.covar_module(x)
+                return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        model = ExactGPModel(train_x, train_y, likelihood)
+        hypers = {
+            'likelihood.noise_covar.noise': torch.tensor(noise_variance),
+            'covar_module.base_kernel.lengthscale': torch.tensor(1.0),
+            'covar_module.outputscale': torch.tensor(var_y),
+        }
+        model.initialize(**hypers)
+        with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=True):
+            logging.info(model.likelihood(model(train_x)).log_prob(train_y))
+
+        train_x = torch.from_numpy(X).float().cuda()
+        train_y = torch.from_numpy(y).float().cuda()
+        likelihood = gpytorch.likelihoods.GaussianLikelihood().cuda()
+        model = ExactGPModel(train_x, train_y, likelihood)
+        hypers = {
+            'likelihood.noise_covar.noise': torch.tensor(noise**2),
+            'covar_module.base_kernel.lengthscale': torch.tensor(1.0),
+            'covar_module.outputscale': torch.tensor(var_y),
+        }
+        model.initialize(**hypers)
+        model = model.cuda()
+        with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=True):
+            logging.info(model.likelihood(model(train_x)).log_prob(train_y))
+
+        N = self.data.N
+        X = self.data.X
+        y = self.data.y
+
+        kernel_skl = var_y * getattr(sklearn.gaussian_process.kernels, "RBF")(length_scale=1.0)
+        gp_skl = GaussianProcessRegressor(kernel_skl,
+                                        alpha=noise_variance,
+                                        optimizer=None,
+                                        copy_X_train=False)
+        gp_skl.fit(X, y)
+        logging.info(gp_skl.log_marginal_likelihood(kernel_skl.theta))
+
+        kernel_gpy = GPy.kern.RBF(input_dim=1, variance=var_y, lengthscale=1.)
+        gp_gpy = GPy.models.GPRegression(X, y, kernel_gpy)
+        gp_gpy.Gaussian_noise.fix(noise_variance)
+        logging.info(gp_gpy.log_likelihood())
+
--- iHOLDR/datasets.py
+++ iHOLDR/datasets.py
@@ -7,42 +7,51 @@ import george
 import math
 from dataclasses import dataclass
 
+from hpolib.benchmarks import synthetic_functions
+# Hacks to get the import to work.
+from hpolib.benchmarks.synthetic_functions.rosenbrock import Rosenbrock5D
+
 @dataclass
 class DataInstance:
-    X: np.ndarray
-    y: np.ndarray
     N: int
+    X: np.ndarray
+    fX: np.ndarray
+    z: np.ndarray
+    y: np.ndarray
     def reshape(self):
         self.X = self.X[:,None]
         self.y = self.y[:,None]
-    def 
 
 class Dataset(common.Component):
-    def __init__(self, **kwargs):
+    def __init__(self, noise_kwargs, n_samples, fn_kwargs, **kwargs):
         super().__init__(**kwargs)
         self.rng = np.random.default_rng(self.random_seed)
+        self.n_samples = n_samples
 
-class ExampleDataset(Dataset):
+        self.noise_kwargs = noise_kwargs
+        self.fn_kwargs = fn_kwargs
+
+    def generate_noise(self, mean, variance):
+        return self.rng.normal(mean, np.sqrt(variance), self.n_samples)
+    def generate_data(self):
+        X, fX = self.generate_X_fX(**self.fn_kwargs)
+        z = self.generate_noise(**self.noise_kwargs)
+        y = fX + z
+        return DataInstance(N=self.n_samples, X=X, fX=fX, z=z, y=y)
+
+class Function(Dataset):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        logging.info(kwargs)
 
-        N = 200
+    def generate_X_fX(self, fn, bounds):
 
-        X = self.rng.uniform(-10, 10, N)
-        noise = 0.3
-        sigma_err = noise ** 2
-        yerr = noise * np.ones_like(X)
-        y = np.sin(X)
-        var_y = np.var(y)
-
-        self.data = DataInstance(X, y, N)
+        if type(fn) == str:
+            _fn = eval(fn)
+        else:
+            _fn = fn
         
-
-        print(len(yerr))
+        low, high = np.swapaxes(bounds, 1, 0)
         
-        self.N = N
-        self.X = X
-        self.y = y
-        self.var_y = var_y
-        self.yerr = yerr
+        X = self.rng.uniform(low, high, self.n_samples)
+        fX = _fn(X)
+        return X, fX
