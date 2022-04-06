3a4382b355acac81a61762865ca0e92e9929e589
Eric Han <Eric.aka.darth_vader@hotmail.com>
2022-04-06 20:07:55 +0800

    Updated gpytorch

--- iHOLDR/algorithms/gpytorch.py
+++ iHOLDR/algorithms/gpytorch.py
@@ -39,26 +39,99 @@ class PyTorchGP(CommonGP):
         self.train_y = torch.from_numpy(self.train_data.y).to(self.output_device)
         self.Kernel = getattr(gpytorch.kernels, kernel)
     def compute_log_likelihood(self):
-        likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.output_device)
-        model = ExactGPModel(self.train_x, self.train_y, likelihood, self.Kernel).to(self.output_device)
-        model.initialize(**self.kernel_kwargs)
+        model, _ = self.create_model()
 
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
             log_likelihood = model.likelihood(model(self.train_x)).log_prob(self.train_y).cpu().numpy()
             return float(log_likelihood)
 
     def predict(self, X):
+        model, likelihood = self.train()
+
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            opt_log_likelihood = model.likelihood(model(self.train_x)).log_prob(self.train_y).cpu().numpy()
+            
+        # Get into evaluation (predictive posterior) mode
+        model.eval()
+        likelihood.eval()
+
+        # Test points are regularly spaced along [0,1]
+        # Make predictions by feeding model through likelihood
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            y_predicted_latent = model(torch.from_numpy(X).to(self.output_device))
+            y_predicted = y_predicted_latent.mean.cpu().numpy()
+
+            opt_kernel_params = np.float64(model.get_outputscale().cpu().numpy()), np.float64(model.get_lengthscale().cpu().numpy())
+
+        return y_predicted, opt_log_likelihood, opt_kernel_params
+
+    def train(self):
+        model, likelihood = self.create_model()
+
+        # Find optimal model hyperparameters
+        model.train()
+        likelihood.train()
+
+        parameters = []
+        for name, param in model.named_parameters():
+            if name.startswith('likelihood'):
+                param.requires_grad = False
+                continue
+            parameters.append(param)
+
+        # Use the adam optimizer
+        optimizer = torch.optim.LBFGS(parameters, **self.optimizer_kwargs)  # Includes GaussianLikelihood parameters
+
+        # "Loss" for GPs - the marginal log likelihood
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+        def closure():
+            optimizer.zero_grad()
+            # Output from model
+            output = model(self.train_x)
+            # Calc loss and backprop gradients
+            loss = -mll(output, self.train_y)
+            loss.backward()
+            return loss
+
+        # Zero gradients from previous iteration
+        loss = optimizer.step(closure)
+
+        logging.info('Final - Loss: %.3f   lengthscale: %.3f   outputscale: %.3f   noise: %.5f' % (
+            loss.item(),
+            model.get_lengthscale().item(),
+            model.get_outputscale().item(),
+            model.likelihood.noise.item()
+        ))
+        
+        # optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+        # # "Loss" for GPs - the marginal log likelihood
+        # mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+        # training_iter = 50
+        # for i in range(training_iter):
+        #     # Zero gradients from previous iteration
+        #     optimizer.zero_grad()
+        #     # Output from model
+        #     output = model(self.train_x)
+        #     # Calc loss and backprop gradients
+        #     loss = -mll(output, self.train_y)
+        #     loss.backward()
+        #     print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+        #         i + 1, training_iter, loss.item(),
+        #         model.covar_module.base_kernel.lengthscale.item(),
+        #         model.likelihood.noise.item()
+        #     ))
+        #     optimizer.step()
+
+        return model, likelihood
+
+    def create_model(self):
         likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.output_device)
         model = ExactGPModel(self.train_x, self.train_y, likelihood, self.Kernel).to(self.output_device)
         model.initialize(**self.kernel_kwargs)
-
-        with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            log_likelihood = model.likelihood(model(self.train_x)).log_prob(self.train_y).cpu().numpy()
-            return float(log_likelihood)
-
-    def train(self):
-        pass
-        
+        return model, likelihood
 
 class ExactAlexanderGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood, Kernel, n_devices, output_device):
@@ -91,11 +164,8 @@ class PyTorchAlexanderGP(PyTorchGP):
     }
     def __init__(self, output_device, preconditioner_size, n_devices, **kwargs):
         if output_device == "cpu":
-            PyTorchAlexanderGP.kernel_kwargs_mapper = {
-                'lengthscale':'covar_module.base_kernel.lengthscale',
-                'noise_variance':'likelihood.noise_covar.noise',
-                'scale_variance':'covar_module.outputscale'
-            }
+            PyTorchAlexanderGP.kernel_kwargs_mapper = PyTorchGP.kernel_kwargs_mapper
+
         super().__init__(output_device=output_device, **kwargs)
 
         self.n_devices = n_devices
@@ -106,8 +176,7 @@ class PyTorchAlexanderGP(PyTorchGP):
 
     def compute_log_likelihood(self):
 
-        likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.output_device)
-        model = self.create_model(likelihood)
+        model, _ = self.create_model()
 
         # gpytorch.settings.fast_pred_var(), 
         with torch.no_grad(), gpytorch.beta_features.checkpoint_kernel(self.checkpoint_size), \
@@ -117,7 +186,7 @@ class PyTorchAlexanderGP(PyTorchGP):
             return np.float64(log_likelihood)
     def predict(self, X):
 
-        model, likelihood = self.train(checkpoint_size=self.checkpoint_size, n_training_iter=15000)
+        model, likelihood = self.train(checkpoint_size=self.checkpoint_size, **self.optimizer_kwargs)
 
         with torch.no_grad(), gpytorch.beta_features.checkpoint_kernel(self.checkpoint_size), \
             gpytorch.settings.max_preconditioner_size(self.preconditioner_size), gpytorch.settings.fast_computations(log_prob=True):
@@ -137,18 +206,19 @@ class PyTorchAlexanderGP(PyTorchGP):
 
         return y_predicted, opt_log_likelihood, opt_kernel_params
 
-    def create_model(self, likelihood):
+    def create_model(self):
+        likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.output_device)
         if str(self.output_device) == "cpu":
             model = ExactGPModel(self.train_x, self.train_y, likelihood, self.Kernel).to(self.output_device)
             model.initialize(**self.kernel_kwargs)
         else:
             model = ExactAlexanderGPModel(self.train_x, self.train_y, likelihood, self.Kernel, self.n_devices, self.output_device).to(self.output_device)
             model.initialize(**self.kernel_kwargs)
-        return model
+        return model, likelihood
 
-    def train(self, checkpoint_size, n_training_iter):
-        likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.output_device)
-        model = self.create_model(likelihood)
+    def train(self, checkpoint_size, max_iter, **optimizer_kwargs):
+
+        model, likelihood = self.create_model()
         likelihood.train()
         model.train()
 
@@ -159,7 +229,7 @@ class PyTorchAlexanderGP(PyTorchGP):
                 continue
             parameters.append(param)
 
-        optimizer = FullBatchLBFGS(parameters, lr=0.1)
+        optimizer = FullBatchLBFGS(parameters, **optimizer_kwargs)
         # "Loss" for GPs - the marginal log likelihood
         mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
 
@@ -178,18 +248,18 @@ class PyTorchAlexanderGP(PyTorchGP):
 
             i = -1
             logging.info('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   outputscale: %.3f   noise: %.5f' % (
-                i + 1, n_training_iter, loss.item(),
+                i + 1, max_iter, loss.item(),
                 model.get_lengthscale().item(),
                 model.get_outputscale().item(),
                 model.likelihood.noise.item()
             ))
 
-            for i in range(n_training_iter):
+            for i in range(max_iter):
                 options = {'closure': closure, 'current_loss': loss, 'max_ls': 10}
                 loss, _, _, _, _, _, _, fail = optimizer.step(options)
 
                 logging.info('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   outputscale: %.3f   noise: %.5f' % (
-                    i + 1, n_training_iter, loss.item(),
+                    i + 1, max_iter, loss.item(),
                     model.get_lengthscale().item(),
                     model.get_outputscale().item(),
                     model.likelihood.noise.item()
@@ -212,7 +282,7 @@ class PyTorchAlexanderGP(PyTorchGP):
             logging.info('Number of devices: {} -- Kernel partition size: {}'.format(self.n_devices, checkpoint_size))
             try:
                 # Try a full forward and backward pass with this setting to check memory usage
-                _, _ = self.train(checkpoint_size=checkpoint_size, n_training_iter=1)
+                _, _ = self.train(checkpoint_size=checkpoint_size, max_iter=1)
 
                 # when successful, break out of for-loop and jump to finally block
                 break
