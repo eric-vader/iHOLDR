9e34ecd402ae6aae75431efb02fb599ae0e4467e
Eric Han <Eric.aka.darth_vader@hotmail.com>
2022-04-21 18:17:05 +0800

    Updated stochastic methods

--- iHOLDR/algorithms/gpy.py
+++ iHOLDR/algorithms/gpy.py
@@ -5,6 +5,7 @@ import logging
 
 from algorithms.commonGP import CommonGP
 
+
 class GPyGP(CommonGP):
     kernel_kwargs_mapper = {
         'lengthscale':'lengthscale',
@@ -15,21 +16,24 @@ class GPyGP(CommonGP):
         data.y = data.y[:,None]
         return data
         
-    def __init__(self, kernel, **kwargs):
+    def __init__(self, kernel, model='GPRegression', model_kwargs={}, **kwargs):
         super().__init__(**kwargs)
         self.Kernel = getattr(GPy.kern, kernel)
         self.kernel_kwargs['input_dim'] = self.train_data.D
         self.noise_variance = self.kernel_kwargs.pop('noise_variance')
 
+        self.Model = getattr(GPy.models, model)
+        self.model_kwargs = model_kwargs
+
     def compute_log_likelihood(self):
         kernel = self.Kernel(**self.kernel_kwargs)
-        model = GPy.models.GPRegression(self.train_data.X, self.train_data.y, kernel)
+        model = self.Model(self.train_data.X, self.train_data.y, kernel, **self.model_kwargs)
         model.Gaussian_noise.fix(self.noise_variance)
-        return model.log_likelihood()
+        return np.float64(model.log_likelihood())
 
     def predict(self, X):
         kernel = self.Kernel(**self.kernel_kwargs)
-        model = GPy.models.GPRegression(self.train_data.X, self.train_data.y, kernel)
+        model = self.Model(self.train_data.X, self.train_data.y, kernel, **self.model_kwargs)
         model.Gaussian_noise.fix(self.noise_variance)
         
         model.optimize(optimizer='lbfgs', max_iters=15000)
