0d13803de34dc39ecd0b55e8175b812c2b429b6b
Eric Han <Eric.aka.darth_vader@hotmail.com>
2022-05-20 17:28:33 +0800

    Updated with curr_id opt

--- iHOLDR/algorithms/george.py
+++ iHOLDR/algorithms/george.py
@@ -165,6 +165,16 @@ class GeorgeGP(CommonGP):
 
     def rearrange_la_kpca_tree(self, model, n_components):
 
+        # K = model.get_matrix(self.train_data.X)
+        # def largest_indices_tril(a):
+        #     m = a.shape[0]
+        #     r,c = np.tril_indices(m,-1)
+        #     idx = a[r,c].argpartition(-1)[-1:]
+        #     return (r[idx][0], c[idx][0])
+
+        # curr_id = largest_indices_tril(K)[0]
+        curr_id = 0
+
         def _kernel(x1, x2):
             return model.kernel.get_value(x1.reshape(1,-1), x2.reshape(1,-1))
 
@@ -173,7 +183,7 @@ class GeorgeGP(CommonGP):
         n_components = self.choose_n_eigvals(pca.eigenvalues_, n_components)
 
         tree = cKDTree(pca_X)
-        d, idx = tree.query(pca_X[0], k=len(pca_X))
+        d, idx = tree.query(pca_X[curr_id], k=len(pca_X))
         self.train_data.rearrange(idx)
 
     def rearrange_la_pca_np(self, model, n_components):
